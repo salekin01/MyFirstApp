06c5c92391fa0e9f2ce51478ce4d0c78f56cc905
salekin01 <salekin01@outlook.com>
2020-04-15 00:52:48 +0200

    added logic for random value & receive the value passed from first fragment

--- app/src/main/java/com/example/myfirstapp/SecondFragment.kt
+++ app/src/main/java/com/example/myfirstapp/SecondFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs  //added
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -21,11 +23,25 @@ class SecondFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_second, container, false)
     }
 
+    private val args: SecondFragmentArgs by navArgs() //added this line to define where the arguments are
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         view.findViewById<Button>(R.id.button_second).setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
+
+        //Added bellow code
+        val count = args.myArg
+        val countText = getString(R.string.random_heading, count)
+        view.findViewById<TextView>(R.id.textview_header).text = countText
+
+        val random = java.util.Random()
+        var randomNumber = 0
+        if (count > 0) {
+            randomNumber = random.nextInt(count + 1)
+        }
+        view.findViewById<TextView>(R.id.textview_random).text = randomNumber.toString()
     }
 }
